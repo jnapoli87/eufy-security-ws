6305c7fd5d867cb2c06f52b51ec1a69847264843
jnapoli87 <73004017+jnapoli87@users.noreply.github.com>
2022-09-25 05:34:11 -0400

    Update message_handler.ts

--- src/lib/station/message_handler.ts
+++ src/lib/station/message_handler.ts
@@ -1,4 +1,4 @@
-import { EufySecurity } from "eufy-security-client";
+import { EufySecurity } from "eufy-security-client-fork";
 
 import { UnknownCommandError } from "../error";
 import { Client } from "../server";
